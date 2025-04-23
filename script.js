997723f2b64b0f15f6ec8f753e9c95a51e2eb6f4
Andrea Lucero <alucerotrasvina@gmail.com>
2025-04-23 15:57:20 -0700

    barra

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+let barraX;
+let barraY = 550;
+let barraAncho = 150;
+let barraAlto = 20;
+let velocidad = 5;
+
 function setup() {
-    createCanvas(1200, 600);
-  }
+  createCanvas(1200, 600);
+  barraX = (width - barraAncho) / 2;
+}
+
+function draw() {
+  background(255);
   
-  function draw() {
-    background(255);
+  //barra ñenga
+  rect(barraX, barraY, barraAncho, barraAlto);
+  
+  //mov
+  if (keyIsDown(LEFT_ARROW)) {
+    barraX -= velocidad;
   }
+  if (keyIsDown(RIGHT_ARROW)) {
+    barraX += velocidad;
+  }
+
+  //limite
+  barraX = constrain(barraX, 0, width - barraAncho);
+}
